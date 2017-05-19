017a29198b06092208d9b4c089bb66b49bf5b378
Chaudhary <chandcha@sch-chandvai.corp.scholasticinc.local>
2017-05-19 13:58:46 -0400

    I am chanda and it is my commit message

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 var newProfile = require( './newProfile' );
 
-console.log( JSON.stringify( newProfile, null, 2 ) );
+// console.log( JSON.stringify( newProfile, null, 2 ) );
+
+//writeNewProfile( { name: 'Chanda' } );
+
+var makeProfile = function( name, employer, title, address ) {
+  var returnObject = {
+    profile: {
+      name: '',
+      employer: {
+        name: '',
+        title: '',
+        address: ''
+      }
+    }
+  };
+
+  returnObject.profile.name = name;
+  returnObject.profile.employer.name = employer;
+  returnObject.profile.employer.title = title;
+  returnObject.profile.employer.address = address;
+
+  return returnObject;
+}
+
+console.log( makeProfile( 'Chanda' , 'Scholastic Inc', 'Quality Czar', '1271 6th Avenue, New York') );
